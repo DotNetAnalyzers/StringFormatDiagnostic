6fb8e4d4378d802d7a2287568a12e41c15b47bb3
AdamSpeight2008 <Adam.Speight@hotmail.co.uk>
2014-05-23 07:22:04 +0100

    Refactoring AnalyzeNode input parameters. Removing those that are now uncessary.
    Added Check for OmittedArgument state
    Eg String.Format( ,0,1,2,3)

--- Diagnostic2/DiagnosticAnalyzer.vb
+++ Diagnostic2/DiagnosticAnalyzer.vb
@@ -51,21 +51,20 @@ Public Class DiagnosticAnalyzer
             If TypeOf fs Is OmittedArgumentSyntax Then Exit Sub
             Dim f = CType(fs, SimpleArgumentSyntax)
             If f IsNot Nothing Then
-              If f.Expression.VisualBasicKind = SyntaxKind.StringLiteralExpression Then
-
-                Dim ReportedIssues = AnalyseFormatString(cancellationToken, Nothing, fs.ToString, Enumerable.Repeat(Of Object)(Nothing, args.Count - 1).ToArray).ToArray
-                For Each ReportedIssue In ReportedIssues
-                  Select Case ReportedIssue
-                    Case cex As ArgIndexOutOfRange            : addDiagnostic(AddWarning(fs, cex.Start, 1 + cex.Finish, ReportedIssue))
-                    Case cex As UnexpectedChar                : addDiagnostic(AddWarning(fs, cex.Start, cex.Start + 1, ReportedIssue))
-                    Case cex As UnexpectedlyReachedEndOfText  : addDiagnostic(AddWarning(fs,0,fs.span.Length,  ReportedIssue))
-                    Case cex As ArgIndexHasExceedLimit        : addDiagnostic(AddWarning(fs, cex.Start,  1 + cex.Finish, ReportedIssue))
-                    Case ___ As Internal_IssueReport          : addDiagnostic(AddWarning(node,0, fs.Span.Length , ReportedIssue))
-                    Case ___ As ContainsNoArgs                : addDiagnostic(AddInformation(fs, "Contains no args! Are you sure this is correct?"))
-                  End Select
-                Next
-              Else
-                If f.Expression.VisualBasicKind = SyntaxKind.IdentifierName Then
+              Select Case f.Expression.VisualBasicKind
+                Case SyntaxKind.StringLiteralExpression
+                  Dim ReportedIssues = AnalyseFormatString(cancellationToken, fs.ToString, args.Count)
+                  For Each ReportedIssue In ReportedIssues
+                    Select Case ReportedIssue
+                      Case cex As ArgIndexOutOfRange : addDiagnostic(AddWarning(fs, cex.Start, 1 + cex.Finish, ReportedIssue))
+                      Case cex As UnexpectedChar : addDiagnostic(AddWarning(fs, cex.Start, cex.Start + 1, ReportedIssue))
+                      Case cex As UnexpectedlyReachedEndOfText : addDiagnostic(AddWarning(fs, 0, fs.Span.Length, ReportedIssue))
+                      Case cex As ArgIndexHasExceedLimit : addDiagnostic(AddWarning(fs, cex.Start, 1 + cex.Finish, ReportedIssue))
+                      Case ___ As Internal_IssueReport : addDiagnostic(AddWarning(node, 0, fs.Span.Length, ReportedIssue))
+                      Case ___ As ContainsNoArgs : addDiagnostic(AddInformation(fs, "Contains no args! Are you sure this is correct?"))
+                    End Select
+                  Next
+                Case SyntaxKind.IdentifierName
                   Dim vx = CType(f.Expression, IdentifierNameSyntax)
                   If vx Is Nothing Then Exit Sub
                   Dim xx = semanticModel.GetConstantValue(vx, cancellationToken)
@@ -78,9 +77,9 @@ Public Class DiagnosticAnalyzer
                   Debugger.Break()
 
                   '
-                End If
 
-              End If
+
+              End Select
 
             End If
 
@@ -97,9 +96,10 @@ Public Class DiagnosticAnalyzer
   Const _LIMIT_ As Integer = 1_000_000  ' This limit is found inside the .net implementation of String.Format.
   Const ExitOnFirst = False
 
-  Iterator Function AnalyseFormatString(cancellationToken As CancellationToken, provider As IFormatProvider, format As String, ParamArray Args() As Object) As IEnumerable(Of IssueReport)
+  Iterator Function AnalyseFormatString(cancellationToken As CancellationToken, format As String, NumOfArgs As Integer) As IEnumerable(Of IssueReport)
+    ' ParamArray Args() As Object) As IEnumerable(Of IssueReport)
     If format Is Nothing Then Throw New ArgumentNullException("fs")
-    If Args Is Nothing Then Throw New ArgumentNullException("Args")
+    'If Args Is Nothing Then Throw New ArgumentNullException("Args")
     '
     ' This is based on the .net framework implementation of String.Format.
     '
@@ -120,7 +120,7 @@ Public Class DiagnosticAnalyzer
     Dim LengthOfTheText = format.Length
     Dim CurrentCharacter = ControlChars.NullChar
     Dim cf As ICustomFormatter = Nothing
-    If provider IsNot Nothing Then cf = CType(provider.GetFormat(GetType(ICustomFormatter)), ICustomFormatter)
+    'If provider IsNot Nothing Then cf = CType(provider.GetFormat(GetType(ICustomFormatter)), ICustomFormatter)
     Try
       While True
         Dim StartPositionForThisPart = 0
@@ -197,9 +197,9 @@ Public Class DiagnosticAnalyzer
           Yield New ArgIndexHasExceedLimit("Arg Index", ArgIndex, _LIMIT_, StartPositionForThisPart, EndPositionForThisPart)
           If ExitOnFirst Then Exit Function
         End If
-        If Not ParsingIsInAnErrorState AndAlso (ArgIndex >= Args.Length) Then
+        If Not ParsingIsInAnErrorState AndAlso (ArgIndex >= NumOfArgs) Then
           ' Index is out of the bounds of the supplied args.
-          Yield New ArgIndexOutOfRange(ArgIndex, Args.Length, StartPositionForThisPart, EndPositionForThisPart)
+          Yield New ArgIndexOutOfRange(ArgIndex, NumOfArgs, StartPositionForThisPart, EndPositionForThisPart)
           ' ToDo: Get the Start and End positions of opening and closing braces.
           If ExitOnFirst Then Exit Function
         End If
@@ -297,9 +297,7 @@ Public Class DiagnosticAnalyzer
         End If
         CurrentPosition += 1
       End While
-      If ArgsCounted = 0 Then
-        Yield New ContainsNoArgs
-      End If
+      If ArgsCounted = 0 Then Yield New ContainsNoArgs
     Catch ex As Exception
       ' Let's use the IDE error window to also report internal errors, :-)
       internalError = New Internal_IssueReport(ex.ToString)
@@ -318,7 +316,7 @@ Public Class DiagnosticAnalyzer
   End Sub
 
   Private Function IsDigit(c As Char) As Boolean
-    Return c >= "0"c AndAlso c <= "9"c
+    Return "0"c <= c  AndAlso c <= "9"c
   End Function
 
   Private Function DigitValue(c As Char) As Integer
