084a3ecb351cde86a28f32c9714ab67e26638041
AdamSpeight2008 <Adam.Speight@Virgin.net>
2014-09-29 01:52:41 +0100

    Restructured Project Updated both VS2013 and VS2014 Improved Error Messages Fix Internal Error

--- StringFormatDiagnostic/DiagnosticAndCodeFix/tools/install.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$analyzerPath = join-path $toolsPath "analyzers"
-$analyzerFilePath = join-path $analyzerPath "String Format Diagnostic (VB).dll"
-
-$project.Object.AnalyzerReferences.Add("$analyzerFilePath")
---
+++ VS2014/VS_Shared/OutputResultOfT/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("OutputResultOfT")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("OutputResultOfT")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8fbaac86-4d29-4c87-9cf1-850917e9ca86")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.13.0.0")> 
+<Assembly: AssemblyFileVersion("1.13.0.0")> 
